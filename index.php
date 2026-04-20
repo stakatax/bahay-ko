3828c6860ff0bdae3144b5cf64953360df2c1389
stakatax <janfortun14@gmail.com>
2026-04-20 17:30:15 +0800

    wag to

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
                 OUR LADY <br>
                 OF SACRED <br>
                 COLLEGE OF <br>
-                GUIMBA, INCs.
+                GUIMBA, INC.
               </h1>
 
               <div class="buttons fade-up fade-up-delay-2">
